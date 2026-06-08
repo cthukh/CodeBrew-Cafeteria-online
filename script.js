e4fa5d7a8a7a3327f978c3ede00b4affca11a6a0
jairo vargas <jairo.vargas.c4@gmail.com>
2026-06-08 03:33:00 -0400

    fixeando

--- script.js
+++ script.js
@@ -4,35 +4,45 @@ let productos = [
         nombre: "Cacao Overclock",
         desc: "El combustible perfecto para tus líneas de código. Una fusión equilibrada de nuestro intenso espresso con cacao amargo de alta pureza, coronado con un arte latte en forma de corazón. Textura cremosa, cuerpo denso y el toque amargo exacto para mantenerte enfocado.",
         precio: 2000,
-        img: "img/cafe_cacao.png"
+        img: "img/cafe_cacao.png",
+        cantidad: 1,
+        subTotal: 0
     },
     {
         id: 2,
         nombre: "Topología de Caramelo",
         desc: "Un espresso suave y cremoso coronado con una delicada malla de caramelo dorado. Esta nueva topologia te permitira sincronizar tu conocimiento con el de grandes programadores. Combinando dulzura, textura y energía en un perfecto equilibrio.",
         precio: 2000,
-        img: "img/cafe_caramel.png"
+        img: "img/cafe_caramel.png",
+        cantidad: 1,
+        subTotal: 0
     },
     {
         id: 3,
         nombre: "Choco Full Stack",
         desc: "Feature sobre Feature. Nuestro espresso de la casa servido sobre una base de chocolate, coronado con una generosa pila de crema batida y virutas de chocolate. El mismisimo Full Stack.",
         precio: 2500,
-        img: "img/cafe_choco.png"
+        img: "img/cafe_choco.png",
+        cantidad: 1,
+        subTotal: 0
     },
     {
         id: 4,
         nombre: "Kernel-Coffee-lts",
         desc: "Un clásico estandarizado. Café filtrado tradicional, balanceado y con el cuerpo ideal para mantener un flujo de trabajo constante. Simple, eficiente y compatible con cualquier rutina.",
         precio: 1000,
-        img: "img/cafe-cafe.png"
+        img: "img/cafe-cafe.png",
+        cantidad: 1,
+        subTotal: 0
     },
     {
         id: 5,
         nombre: "Ctrl + Té",
         desc: "El reinicio mental que necesitas cuando el entorno se llena de bugs. Una infusión herbal milenaria diseñada para alinear tus chakras, limpiar la caché de tu cerebro y restaurar tu estabilidad emocional tras un error de sintaxis inesperado.",
         precio: 1000,
-        img: "img/cafe-te.png"
+        img: "img/cafe-te.png",
+        cantidad: 1,
+        subTotal: 0
     },
 ]
 
@@ -42,56 +52,164 @@ const containerCarrousel = document.getElementById("carrousel");
 const select = document.getElementById("inputCafe");
 
 
-for (let i = 0; i < productos.length; i++) {
-    const producto = productos[i];
-    
-    const slide = document.createElement('div');
-    slide.classList.add("slide");
+function render_carousel_opciones() {
 
-    const textSlide = document.createElement("div");
-    textSlide.classList.add('text-slide');
-    textSlide.classList.add('col-4');
+    for (let i = 0; i < productos.length; i++) {
+        const producto = productos[i];
+        
+        const slide = document.createElement('div');
+        slide.classList.add("slide");
+        
+        const textSlide = document.createElement("div");
+        textSlide.classList.add('text-slide');
+        textSlide.classList.add('col-4');
+        
+        const h2 = document.createElement('h2');
+        h2.textContent = producto.nombre;
 
-    const h2 = document.createElement('h2');
-    h2.textContent = producto.nombre;
-
-    const p = document.createElement('p')
-    p.textContent = producto.desc;
-
-    const img = document.createElement('img');
-    img.classList.add('img-slide');
-    img.classList.add('col-4');
-    img.src = producto.img;
-    img.alt = producto.nombre;
-
-    textSlide.append(h2,p);
-    slide.append(textSlide,img);
-    containerCarrousel.append(slide);
+        const p = document.createElement('p')
+        p.textContent = producto.desc;
+        
+        const img = document.createElement('img');
+        img.classList.add('img-slide');
+        img.classList.add('col-4');
+        img.src = producto.img;
+        img.alt = producto.nombre;
+        
+        textSlide.append(h2,p);
+        slide.append(textSlide,img);
+        containerCarrousel.append(slide);
 
 
-    const option = document.createElement('option');
-    option.value = producto.id;
-    option.text = producto.nombre;
+        const option = document.createElement('option');
+        option.value = producto.id;
+        option.text = producto.nombre;
+        
+        select.append(option);
+    }
+};
 
-    select.append(option);
-}
+render_carousel_opciones()
 
-const btnComprar = document.getElementById('buttonSelectCafe');
-btnComprar.addEventListener('click', function () {
-    let select = document.getElementById('inputCafe');
+
+
+// Carrito de compras
+let carrito = [ ];
+let contadorProductos = 0;
+let total = 0;
+let coincidencia = false;
+
+function agregar_al_carro() {
+    const listaProductos = document.getElementById('listaProductos');
+    const select = document.getElementById("inputCafe");
+    const tabla = document.getElementById('tablaDatos');
     let opcion = select.value;
+
+
     if (opcion >= 1 && opcion <= 5 ) { // Bueno
-        console.log('valido')
+        console.log("----------------------")
+        console.log('Inicio')
+        // Ocultar texto
         const texto = document.getElementById('TextOtter');
+
+        if (texto.classList != 'd-none') {
+            texto.classList.add('d-none');
+        };
+
+        listaProductos.classList.remove('d-none');
+
         for (let n = 0; n < productos.length; n++) {
             const producto = productos[n];
-            if (producto.id == opcion) {
-                texto.innerHTML = `producto: ${producto.nombre}`;
-            }
-        }
-        
 
-    } else { // Malo
-        console.log("MAlo")
+            if (producto.id == opcion) {
+                console.log("Producto encontrado")
+
+                // Verificar si ya esta el producto en el carrito
+                if (carrito.length == 0 ) {
+                    console.log("Carrito vacio!")
+                    agregar_producto(producto,tabla)
+                    break
+                };
+
+                if (carrito.length > 0) {
+                    for (let c = 0; c < carrito.length; c++ ) {
+                        const productoInCarrito = carrito[c];
+                        console.log("Mostrando productoInCarrito: "+productoInCarrito.nombre)
+                        console.log("Mostrando vueltas de ciclo de busqueda en carrito: "+c)
+
+                        if (producto.nombre == productoInCarrito.nombre) {
+                            coincidencia = true
+                            console.log("Carrito con productos. Se encontra ya un producto en el carro: "+producto.nombre )
+                            modificar_cantidad(producto)
+                            break
+                            
+                        } else {
+                            coincidencia = false
+                            console.log(coincidencia)
+                        };
+                    };
+
+                    if (coincidencia == false) {
+                        console.log("Carrito con productos, pero sin coincidencia: "+producto.nombre )
+                        agregar_producto(producto,tabla)
+                        break
+                    };
+                };
+            };
+        };
+    console.log(carrito)
+    console.log("fin funcion ------------------------")
+
+
+    } else { // Sin valor
+        alert("Seleccione un producto para agregar al carro.")
+        console.log("No se seleccionó Producto.")
     };
-});
+};
+
+
+function modificar_cantidad(producto) {
+    console.log("Producto ya en el carro. Modificando cantidad...")
+
+    producto.cantidad = producto.cantidad + 1;
+    let id_cant = 'CantidadId'+producto.id;
+    const colCant = document.getElementById(id_cant);
+    colCant.innerHTML = producto.cantidad;
+    
+    producto.subTotal = producto.precio * producto.cantidad;
+    console.log("Mostrando sub total del producto "+producto.nombre+" sub-total: "+producto.subTotal)
+    let id_sub = 'SubTotalId'+producto.id;
+    const colSub = document.getElementById(id_sub);
+    colSub.innerHTML = producto.subTotal;
+};
+
+function agregar_producto(producto,tabla) {
+    console.log("Producto no encontrado en carrito. Agregando...")
+    let subTotal = producto.precio * producto.cantidad;
+    producto.subTotal = subTotal;
+    contadorProductos ++
+
+    const fila = document.createElement('tr');
+
+    const colNum = document.createElement('td');
+    colNum.textContent = contadorProductos;
+
+    const colNombre = document.createElement('td');
+    colNombre.textContent = producto.nombre;
+
+    const colCantidad = document.createElement('td');
+    colCantidad.id = 'CantidadId'+producto.id;
+    colCantidad.textContent = producto.cantidad;
+
+    const colPrecio = document.createElement('td');
+    colPrecio.textContent = producto.precio;
+
+    const colSubTotal = document.createElement('td');
+    colSubTotal.id = 'SubTotalId'+producto.id
+    colSubTotal.textContent = producto.subTotal;
+
+    fila.append(colNum, colNombre, colCantidad, colPrecio, colSubTotal);
+    tabla.append(fila);
+
+    carrito.push(producto);
+};
